01a9bbccbdebd002b18df7efd0a4872ef0f23916
Arnaiz23 <aaf110501@gmail.com>
2023-05-21 15:12:01 +0200

    feat: 🎸 continue with the course

---
+++ basics/if.py
@@ -0,0 +1,10 @@
+edad = int(input("Edad?: "))
+
+if edad > 54:
+    print("Puede ver la película con descuento")
+elif edad > 17:
+    print("Puedes ver la película")
+else:
+    print("No puedes ver la película")
+
+print("Fin")
