ba18e53881b905f5469f45bed1dd0911aa4260e7
Arnaiz23 <aaf110501@gmail.com>
2023-07-26 02:09:19 +0200

    feat: Use an array for open the actualMangas

--- actualManga/index.py
+++ actualManga/index.py
@@ -1,5 +1,19 @@
 import webbrowser
 
-url = "https://lectortmo.com/library/manhwa/53005/trash-of-the-counts-family?orderDir=ASC"
+urls = [
+    "https://lectortmo.com/library/manga/78340/el-sabio-desterrado-creo-una-clase-invencible",
+    "https://lectortmo.com/library/manhwa/53005/trash-of-the-counts-family",
+]
 
-webbrowser.open(url, new=0, autoraise=True)
+print("Available mangas: ")
+
+for index, url in enumerate(urls):
+    url_split = url.split("/")
+    last_position = url_split[len(url_split) - 1]
+    manga_name_dict = last_position.split("-")
+    manga_name = " ".join(manga_name_dict)
+    print(f"{index}. {manga_name.capitalize()}")
+
+option = int(input("Select a manga: "))
+
+webbrowser.open(urls[option], new=0, autoraise=True)
